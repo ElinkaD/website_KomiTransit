5999c91f0fb070bd9be4f6bbc130ee94cc94b8f3
elina <dusaevaee@mail.ru>
2024-06-16 21:35:42 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+	<title>ООО Коми Транзит</title>
+	<link rel="stylesheet" href="css/style.css" />
+	<meta charset="UTF-8" />
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+</head>
+
+<body>
+	<header class="main-header">
+	</header>
+	<main>
+	</main>
+	<footer class="main-footer">
+	</footer>
+</body>
+
+</html>
