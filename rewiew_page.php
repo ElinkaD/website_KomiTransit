81527fc4b36dd72468714604edcd340a01c7a264
elina <dusaevaee@mail.ru>
2024-06-18 21:53:21 +0300

    rewiew_page

---
+++ rewiew_page.php
@@ -0,0 +1,51 @@
+<head>
+    <link rel="stylesheet" href="css/rewiew_page.css" />
+</head>
+
+<section class="hero">
+    <div class="hero-text">
+        <a href="index.php?page=main_page"><h4>Главная</h4></a>
+        <h1>Отзывы</h1>
+    </div>
+</section>
+
+<div class="page">
+    <section class="review">
+            <h3>Что о нас говорят клиенты</h3>
+            <div class="rows">
+                <article class="card">
+                    <div class="frame">
+                        <div class="name">Иван Петров</div>
+                        <p>«ООО Коми Транзит» превзошли все наши ожидания! Мы доставляли негабаритное оборудование на нефтяные промыслы, и их железнодорожный тупик оказался идеальным решением. Всё прошло быстро и безупречно. Рекомендуем!</p>
+                        <hr>
+                        <div class="date">
+                            <p>25 мая 2024 г.</p>
+                        </div>
+                    </div>
+                </article>
+                <article class="card">
+                    <div class="frame">
+                        <div class="name">Анна Смирнова</div>
+                        <p>Отличный сервис от компании «Коми Транзит». Благодаря прямому сотрудничеству с железнодорожной станцией Усинск, мы смогли сэкономить время и средства. Качество услуг на высшем уровне!</p>
+                        <hr>
+                        <div class="date">
+                            <p>25 мая 2024 г.</p>
+                        </div>
+                    </div>
+                </article>
+                <article class="card">
+                    <div class="frame">
+                        <div class="name">Максим Ковалев</div>
+                        <p>Мы долго искали надёжного партнёра для транспортировки негабаритного груза, и ООО Коми Транзит стали для нас находкой. Их прозрачные тарифы сделали наше сотрудничество максимально комфортным. Спасибо большое за ваш труд!</p>
+                        <hr>
+                        <div class="date">
+                            <p>25 мая 2024 г.</p>
+                        </div>
+                    </div>
+                </article>
+            </div>
+        </section>
+</div>
+
+
+</div>
